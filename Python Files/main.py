cb34df76ceb76afea79264ac6a4144363230c0dd
Laerte Xavier <laertexavier@gmail.com>
2020-08-29 12:37:24 -0300

    teste

--- Python Files/main.py
+++ Python Files/main.py
@@ -2,3 +2,7 @@
 #@body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
 def hello():
     print("Hello")
+
+#todo method to solve problem
+def g():
+    pass
